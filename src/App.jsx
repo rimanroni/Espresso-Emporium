e7a71c015360bd479f5f506ccce288b49fea55ef
roni rimane <rimanroni386@gmail.comm>
2024-09-16 16:33:27 +0600

    start projects

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+ 
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [user, setUser] = useState([])
+  useEffect(()=>{
+    fetch('http://localhost:5000/user')
+    .then(res=>res.json())
+    .then(data=>setUser(data))
+  })
+ 
+  return (
+    <>
+    <h1 className="text-3xl font-bold text-center ">Total User : {user.length}</h1>
+    </>
+  )
+}
+
+export default App
